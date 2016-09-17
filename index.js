9f05edc9d4ebfb262d84d46ab21b1a6b2ef8bf44
Manos Kontakis <emkontakis@gmail.com>
2016-09-17 17:33:20 +0200

    Added Server

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-/**
- * Created by manolis on 17-Sep-16.
- */
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(8080, 'APP_PRIVATE_IP_ADDRESS');
+console.log('Server running at http://APP_PRIVATE_IP_ADDRESS:8080/');
