232271c13c433fe4e7e0a2d3b1a2640de25368f4
dummyDev <cotton.club098@gmail.com>
2016-09-30 11:19:16 +0200

    Updated Front-End

--- views/js/scripts.js
+++ views/js/scripts.js
@@ -1,6 +1,13 @@
 // Menu Hover effect
 $('ul>li>a').hover(function(){
  $('ul>li>a').not(this).toggleClass('toggle');
-    })
+    });
 
-//$('#myModal').modal('show');
+
+$(document).ready(function() {
+
+    if(window.location.href.indexOf('#gaModal') != -1) {
+        $('#gaModal').modal('show');
+    }
+
+});
